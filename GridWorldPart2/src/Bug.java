1902d0e6b31406db9b9a71e989f031e13d519b47
rdevanathan686 <32176984+rdevanathan686@users.noreply.github.com>
2017-11-10 15:16:35 -0800

    BoxBug Code
    subclass created using the bug class

--- GridWorldPart2/src/Bug.java
+++ GridWorldPart2/src/Bug.java
@@ -14,8 +14,8 @@
  * @author Cay Horstmann
  */
 
-import info.gridworld.actor.Actor;
-import info.gridworld.actor.Flower;
+package info.gridworld.actor;
+
 import info.gridworld.grid.Grid;
 import info.gridworld.grid.Location;
 
@@ -28,107 +28,78 @@ import java.awt.Color;
  */
 public class Bug extends Actor
 {
-   private Color initialColor;
-   private boolean previousMove = false;
-   private final Color PURPLE_COLOR = new Color(148,0,211);
-   /**
-    * Constructs a red bug.
-    */
-   public Bug()
-   {
-      initialColor = Color.RED;
-      setColor(initialColor);
-   }
+    /**
+     * Constructs a red bug.
+     */
+    public Bug()
+    {
+        setColor(Color.RED);
+    }
 
-   /**
-    * Constructs a bug of a given color.
-    * @param bugColor the color for this bug
-    */
-   public Bug(Color bugColor)
-   {
-      initialColor = bugColor;
-      setColor(initialColor);
+    /**
+     * Constructs a bug of a given color.
+     * @param bugColor the color for this bug
+     */
+    public Bug(Color bugColor)
+    {
+        setColor(bugColor);
+    }
 
-   }
-   /**
-    * Moves if it can move, turns otherwise.
-    */
-   public void act()
-   {
-    
-      if (canMove())
-      {
-         if (previousMove)
-         {
-            setColor(initialColor);
-            previousMove = false;
-         }
-            
-         move();
-      }
-      else
-      {
-         turn();
-         turn();
-         
-         if (!previousMove)
-         {
-            initialColor = getColor();
-            setColor(PURPLE_COLOR);
-            previousMove = true;
-            
-         }
-         
-      }
+    /**
+     * Moves if it can move, turns otherwise.
+     */
+    public void act()
+    {
+        if (canMove())
+            move();
+        else
+            turn();
+    }
 
-   }
-   
-   
-   
-   /**
-    * Turns the bug 45 degrees to the right without changing its location.
-    */
-   public void turn()
-   {
-      setDirection(getDirection() + Location.HALF_RIGHT);
-   }
+    /**
+     * Turns the bug 45 degrees to the right without changing its location.
+     */
+    public void turn()
+    {
+        setDirection(getDirection() + Location.HALF_RIGHT);
+    }
 
-   /**
-    * Moves the bug forward, putting a flower into the location it previously
-    * occupied.
-    */
-   public void move()
-   {
-      Grid<Actor> gr = getGrid();
-      if (gr == null)
-         return;
-      Location loc = getLocation();
-      Location next = loc.getAdjacentLocation(getDirection());
-      if (gr.isValid(next))
-         moveTo(next);
-      else
-         removeSelfFromGrid();
-      Flower flower = new Flower(getColor());
-      flower.putSelfInGrid(gr, loc);
-   }
+    /**
+     * Moves the bug forward, putting a flower into the location it previously
+     * occupied.
+     */
+    public void move()
+    {
+        Grid<Actor> gr = getGrid();
+        if (gr == null)
+            return;
+        Location loc = getLocation();
+        Location next = loc.getAdjacentLocation(getDirection());
+        if (gr.isValid(next))
+            moveTo(next);
+        else
+            removeSelfFromGrid();
+        Flower flower = new Flower(getColor());
+        flower.putSelfInGrid(gr, loc);
+    }
 
-   /**
-    * Tests whether this bug can move forward into a location that is empty or
-    * contains a flower.
-    * @return true if this bug can move.
-    */
-   public boolean canMove()
-   {
-      Grid<Actor> gr = getGrid();
-      if (gr == null)
-         return false;
-      Location loc = getLocation();
-      Location next = loc.getAdjacentLocation(getDirection());
-      if (!gr.isValid(next))
-         return false;
-      Actor neighbor = gr.get(next);
-      return (neighbor == null) || (neighbor instanceof Flower);
-      // ok to move into empty location or onto flower
-      // not ok to move onto any other actor
-   }
+    /**
+     * Tests whether this bug can move forward into a location that is empty or
+     * contains a flower.
+     * @return true if this bug can move.
+     */
+    public boolean canMove()
+    {
+        Grid<Actor> gr = getGrid();
+        if (gr == null)
+            return false;
+        Location loc = getLocation();
+        Location next = loc.getAdjacentLocation(getDirection());
+        if (!gr.isValid(next))
+            return false;
+        Actor neighbor = gr.get(next);
+        return (neighbor == null) || (neighbor instanceof Flower);
+        // ok to move into empty location or onto flower
+        // not ok to move onto any other actor
+    }
 }
