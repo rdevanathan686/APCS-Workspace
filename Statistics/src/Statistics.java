771f9937d86b542f81d86dcd719a4b9bbba31047
rdevanathan686 <rdevanathan686@student.fuhsd.org>
2017-11-22 10:50:46 -0800

    Lab Statistics Almost Working

--- Statistics/src/Statistics.java
+++ Statistics/src/Statistics.java
@@ -1,15 +1,15 @@
-
 public class Statistics
 {
     private int[] data;
     private double average;
-    private int mode;
+    private int[] mode;
     private double stdDiv;
     
     
     public Statistics(int maxLength)
     {
         data = new int[maxLength];
+        mode = new int[maxLength];
     }
     
     public void readData(String fileName)
@@ -22,58 +22,89 @@ public class Statistics
     {
         for(int num : data)
             System.out.println(num);
-
+    }
+    
+    public void printData(int[] inputData)
+    {
+        for(int num : inputData)
+            System.out.println(num);
     }
     
     public void calcAvg()
     {
         long localAvg = 0;
         
-        for(int num : data)
+        for (int num : data)
             localAvg += num;
 
-        average = localAvg / data.length;
+        average = localAvg / (double)(data.length);
         
     }
     
-    public void calcMode()
+    public int[] calcMode()
     {
         int maxOccurence = 0;
-        int localMode = 0;
+        int numModes = 0;
+        int[] localMode = new int[numModes + 1];
+
         
         for(int i : data)
         {
            int iOcc = 0;
            
            for(int j : data)
-           {
                if (i == j)
                    iOcc++;
-           }
-           
-           if (maxOccurence < iOcc)
+                         
+           if (maxOccurence <= iOcc)
            {
-               maxOccurence = iOcc;
-               localMode = i;
+               boolean exists = false;
+               
+               for (int k = 0; k < localMode.length; k++)
+               {
+                   if (localMode[k] == i)
+                   {
+                       exists = true;
+                       break;
+                   }
+               }
+               
+               if (!exists)
+               {
+                   numModes++;
+                   maxOccurence = iOcc;
+                   
+                   int[] copyLocalMode = new int[localMode.length + 1];
+                   
+                   for (int l = 0; l < localMode.length; l++)
+                   {
+                       copyLocalMode[l] = localMode[l];
+                   }
+                   
+                   copyLocalMode[numModes] = i;
+                   
+                   localMode = copyLocalMode;
+
+               }
+
            }
 
         }
         
-        mode = localMode;
+        return localMode;
     }
     
-    public void calcStdDiv()
+    public void calcStdDev()
     {
+        calcAvg();
+        
         int localStdDiv = 0;
         
         for(int num : data)
-        {
-            localStdDiv += (average - num);
-        }
+            localStdDiv += ((average - num) * (average - num));
         
-        localStdDiv = localStdDiv / (data.length - 1);
+        stdDiv = Math.sqrt(localStdDiv / (double)(data.length - 1));
         
-        stdDiv = Math.sqrt(localStdDiv);
     }
     
     public int[] getData()
@@ -81,39 +112,23 @@ public class Statistics
         return data;
     }
 
-    public void setData(int[] data)
-    {
-        this.data = data;
-    }
-
     public double getAverage()
     {
-        return average;
+        calcAvg();
+        return (double) Math.round(average * 100) / 100;
     }
 
-    public void setAverage(double average)
+    public int[] getMode()
     {
-        this.average = average;
-    }
-
-    public int getMode()
-    {
+        calcMode();
         return mode;
     }
 
-    public void setMode(int mode)
+    public double getStdDev()
     {
-        this.mode = mode;
+        calcStdDev();
+        return (double) Math.round(stdDiv * 100) / 100;
     }
 
-    public double getStdDiv()
-    {
-        return stdDiv;
-    }
-
-    public void setStdDiv(double stdDiv)
-    {
-        this.stdDiv = stdDiv;
-    }
 
 }
--- Statistics/src/StatisticsRunner.java
+++ Statistics/src/StatisticsRunner.java
@@ -3,10 +3,19 @@ public class StatisticsRunner
 {
     public static void main (String[] args)
     {
-        Statistics numberTester = new Statistics(1000);
-        numberTester.readData("numbers_1.txt");
-        numberTester.calcStdDiv();
-        System.out.println(numberTester.getStdDiv());
+        Statistics numberTester = new Statistics(26);
+        numberTester.readData("test.txt");
         
+        System.out.println("For the following data: ");
+        numberTester.printData();
+        
+        System.out.println("The average is " + numberTester.getAverage());
+        System.out.println("The standard deviation is " + numberTester.getStdDev());
+
+        System.out.println("The mode(s) is: ");
+        numberTester.printData(numberTester.getMode());
+
+        
+
     }
 }
