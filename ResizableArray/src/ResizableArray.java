5ac3bdc50aa2da613e899dbc0ba873664b6ef440
rdevanathan686 <rdevanathan686@student.fuhsd.org>
2017-12-06 21:49:42 -0800

    commit test

--- ResizableArray/src/ResizableArray.java
+++ ResizableArray/src/ResizableArray.java
@@ -2,49 +2,49 @@ import java.util.Arrays;
 
 public class ResizableArray
 {
-     private int[] data;
-     private int size;
-     private final int DEFAULT_LENGTH = 50;
-     
-     public ResizableArray()
-     {
-         data = new int[DEFAULT_LENGTH];
-         size = 0;
-     }
-     
-     public void add(int value)
-     {
-         if (data.length == size)
-             resize();
-         
-         data[size] = value;
-         size++;
-     }
-     
-     private void resize()
-     {
-         int[] resizedData = Arrays.copyOf(data, DEFAULT_LENGTH + 50);
-         data = resizedData;
-     }
-     
-     public void removeAll(int value)
-     {
-         
-         for(int i = 0; i < size; i++)
-         { 
-             if (data[i] == value)
-             {
-                 for(int j = i; j < size - 1; j++)
-                     data[j] = data[j + 1];
-                 
-                 size--;
-                 i--;   
-             }
-         }
-     }
-     
-     public int remove(int index)
-     {
+    private int[] data;
+    private int size;
+    private final int DEFAULT_LENGTH = 50;
+
+    public ResizableArray()
+    {
+        data = new int[DEFAULT_LENGTH];
+        size = 0;
+    }
+
+    public void add(int value)
+    {
+        if (data.length == size)
+            resize();
+
+        data[size] = value;
+        size++;
+    }
+
+    private void resize()
+    {
+        int[] resizedData = Arrays.copyOf(data, DEFAULT_LENGTH + 50);
+        data = resizedData;
+    }
+
+    public void removeAll(int value)
+    {
+
+        for (int i = 0; i < size; i++)
+        {
+            if (data[i] == value)
+            {
+                for (int j = i; j < size - 1; j++)
+                    data[j] = data[j + 1];
+
+                size--;
+                i--;
+            }
+        }
+    }
+
+    public int remove(int index)
+    {
 
         if (index >= size || index < 0)
             throw new IllegalArgumentException("Index out of range of the data");
@@ -60,42 +60,40 @@ public class ResizableArray
 
     }
 
-     public String toString()
-     {
-         String output = "[";
-         
-         for(int i = 0; i < size; i++)
-         {
-             int num = data[i];
-             
-             if (i != size - 1)
-                 output += (num + ", ");
-             else
-                 output += (num + "]");
-         }
+    public String toString()
+    {
+        String output = "[";
 
-         
-         return output;
+        for (int i = 0; i < size; i++)
+        {
+            int num = data[i];
 
-     }
-     
-     public int size()
-     {
-         return size;
-     }
+            if (i != size - 1)
+                output += (num + ", ");
+            else output += (num + "]");
+        }
+
+        return output;
+
+    }
+
+    public int size()
+    {
+        return size;
+    }
 
     public void insert(int index, int value)
     {
         if (index > size || index < 0)
             throw new IllegalArgumentException("Index out of range of the data");
-        
+
         size++;
-        
+
         for (int i = size - 1; i > index; i--)
         {
             data[i] = data[i - 1];
         }
-        
+
         data[index] = value;
     }
 
@@ -108,25 +106,35 @@ public class ResizableArray
     {
         return data[index];
     }
-    
+
     public boolean equals(Object other)
     {
         if (other instanceof ResizableArray)
         {
-            ResizableArray other2 = (ResizableArray)(other);
+            ResizableArray other2 = (ResizableArray) (other);
+
+            if (data.length != other2.data.length)
+                return false;
+
+            for (int i = 0; i < data.length; i++)
+            {
+                if (data[i] != other2.data[i])
+                {
+                    return false;
+                }
+            }
             
-            //TODO
-            return Arrays.equals(other2.data, this.data);
+            return true;
         }
         
         return false;
-        
+
     }
 
     public void sort()
     {
         // TODO Auto-generated method stub
-        
+
     }
 
 }
--- ResizableArray/src/Tester.java
+++ ResizableArray/src/Tester.java
@@ -10,9 +10,17 @@ public class Tester
         arr.add(10);
         arr.add(10);
         arr.insert(0, 9);
+        
+        ResizableArray arr2 = new ResizableArray();
+        arr2.add(10);
+        arr2.add(40);
+        arr2.add(40);
+        arr2.add(10);
+        arr2.add(1);
+        arr2.insert(0, 9);
      
         //arr.removeAll(10);
         
-        System.out.println(arr);
+        System.out.println(arr.equals(arr2));
     }
 }
